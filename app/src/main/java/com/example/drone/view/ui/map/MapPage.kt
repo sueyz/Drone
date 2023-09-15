b5272c59162fd6ec15b0da19b6993569b961d87b
sueazmin <sueazmin@gmail.com>
2023-09-16 03:34:09 +1000

    added map view
    - hide status bar

--- app/src/main/java/com/example/drone/view/ui/map/MapPage.kt
+++ app/src/main/java/com/example/drone/view/ui/map/MapPage.kt
@@ -1,7 +1,13 @@
 package com.example.drone.view.ui.map
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.example.drone.R
 import com.example.drone.base.BaseFragment
 import com.example.drone.databinding.FragmentMapPageBinding
@@ -13,7 +19,31 @@ class MapPage : BaseFragment<FragmentMapPageBinding, HomeViewModel>() {
     //by viewmodel scope
     override val viewModel: HomeViewModel by viewModels()
 
-    override fun init(savedInstanceState: Bundle?) {}
+    override fun init(savedInstanceState: Bundle?) {
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+
+        val window = activityContext.window
+
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+    }
 
     override fun initView(savedInstanceState: Bundle?) {}
+
+    override fun setupView(view: View) {
+        navController = Navigation.findNavController(view)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+    }
 }
