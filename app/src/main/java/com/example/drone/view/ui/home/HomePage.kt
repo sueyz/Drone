829e2cfc0c39106fa8b05ae4756e3c4a38b866fd
sueazmin <sueazmin@gmail.com>
2023-09-16 02:27:31 +1000

    cleanup

--- app/src/main/java/com/example/drone/view/ui/home/HomePage.kt
+++ app/src/main/java/com/example/drone/view/ui/home/HomePage.kt
@@ -3,9 +3,11 @@ package com.example.drone.view.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.example.drone.R
 import com.example.drone.base.BaseFragment
 import com.example.drone.databinding.FragmentHomePageBinding
+import safeNavigate
 
 class HomePage : BaseFragment<FragmentHomePageBinding, HomeViewModel>(){
 
@@ -19,5 +21,12 @@ class HomePage : BaseFragment<FragmentHomePageBinding, HomeViewModel>(){
 
     override fun setupView(view: View) {
         deviceBackButtonGoToHome()
+
+        navController = Navigation.findNavController(view)
+
+        binding.btStart.setOnClickListener {
+            navController.safeNavigate(R.id.action_homePage_to_mapPage)
+        }
+
     }
 }
