84146eccd7db093b490d606956d9ec55a6d46744
sueazmin <sueazmin@gmail.com>
2023-09-19 04:10:03 +1000

    update minor

--- app/src/main/java/com/example/drone/data/network/WeatherAPIService.kt
+++ app/src/main/java/com/example/drone/data/network/WeatherAPIService.kt
@@ -5,13 +5,15 @@ import io.reactivex.rxjava3.core.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.drone.BuildConfig
+
 
 class WeatherAPIService {
 
     val UNITS = "metric"
 
     private val BASE_URL = "https://api.openweathermap.org/"
-    private val API_KEY = "55c9fe40b417db1831f8db9be0bbc768"
+    private val API_KEY = BuildConfig.API_KEY
 
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
@@ -22,7 +24,7 @@ class WeatherAPIService {
 
     fun getDataService(lat: String, lon: String): Single<Response> {
         if (API_KEY.isNullOrBlank()) throw Exception("Add api key")
-        return api.getData(lat,lon, API_KEY, UNITS)
+        return api.getData(lat,lon, API_KEY , UNITS)
     }
 
 }
